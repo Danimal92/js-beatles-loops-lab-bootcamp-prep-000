78a8c7fee01ebd67b0960a647a602c34d0affb24
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-04 23:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-theBeatlesPlay(musicians,instruments){
+function theBeatlesPlay(musicians,instruments){
   var result = []
   for (i=0;i<musicians.length;i++){
     result.push(`${musicians[i]} plays ${instruments[i]}`)
