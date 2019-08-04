a056b32c2823fbc91b9173d0dae5e6ea8ad1098b
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-04 23:17:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts(facts){
   var arr = [];
   var i = 0;
   while(i < facts.length){
-    arr.push(`${facts[i]}!!!`)
+    arr.push(facts[i] + '!!!')
     i++
   }
   return arr
